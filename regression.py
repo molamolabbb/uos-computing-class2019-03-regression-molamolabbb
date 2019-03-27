c5a9150d06701ad0a21a62efd78ef7e64018d98e
Ian J. Watson <ian.james.watson@cern.ch>
2019-03-27 14:35:56 +0900

    Expand format a bit

--- regression.py
+++ regression.py
@@ -1,15 +1,19 @@
 import csv
 
-# Your code for regression here
+# Your code for regression and R2 here
 
 if __name__ == "__main__":
     # Here, we load the boston dataset
     boston = csv.reader(open('boston.csv'))  # The boston housing dataset in csv format
-    header = boston.next()  # First line contains the header, short info for each variable
-    data, target = [], []  # Data will hold the 13 data variables, target is what we are trying to predict
+    # First line contains the header, short info for each variable
+    header = boston.__next__()  # In python2, you might need boston.next() instead
+    # Data will hold the 13 data variables, target is what we are trying to predict
+    data, target = [], []
     for row in boston:
-        data.append(row[:-1])  # All but the last are the data points
-        target.append(row[-1]) # The last is the median house value we are trying to predict
+        # All but the last are the data points
+        data.append([float(r) for r in row[:-1]])
+        # The last is the median house value we are trying to predict
+        target.append(float(row[-1])) 
     # Now, use the dataset with your regression functions to answer the exercise questions
     print("Names of the columns")
     print(header)
@@ -21,6 +25,9 @@ if __name__ == "__main__":
     # Example of writing out the R2.txt file, with 0.0 guess for coefficient of correlation
     fout = open('R2.txt', 'w')
     for i in range(13):
+        column = [row[i] for row in data]  # get the column
+        target  # target is always the median house value
+        alpha, beta = 0.0, 0.0
         R2 = 0.0  # Fill with the real value from your code
-        fout.write('%f\n' % R2)  # One line per variable
+        fout.write('%f,%f,%f\n' % (alpha, beta, R2))  # One line per variable
     fout.close()
