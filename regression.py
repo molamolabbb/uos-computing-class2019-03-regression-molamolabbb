eea9b1205f910d585fc104a9e68b142b005e77d8
molamolabbb <molamolabbb@gmail.com>
2019-03-29 09:56:44 +0900

    Update regression.py

--- regression.py
+++ regression.py
@@ -9,6 +9,36 @@ import matplotlib.pyplot as plt
 import math 
 # Your code for regression and R2 here
 
+def predict(alpha, beta, x_i):
+    return alpha+beta*x_i
+
+def mean(x):
+    return float(sum(x))/len(x)
+            
+def variance(x):
+    x_2 = [i**2 for i in x]
+    return float(mean(x_2)-mean(x)**2)
+def covariance(x,y):
+    x_y = [i*j for i,j in zip(x,y)]
+    return mean(x_y)-mean(x)*mean(y)
+
+def correlation(x,y):
+    return float(covariance(x,y)/(math.sqrt(variance(x))*math.sqrt(variance(y))))
+
+def linear_regression_least_squares(x,y):
+    beta = covariance(x,y)/variance(x)
+    alpha = mean(y)-beta*mean(x)
+    return float(alpha), float(beta)
+    
+def r_squared(alpha, beta, x, y):
+    ss_tot = 0
+    ss_res = 0
+    m = mean(y)
+    ss_tot = sum([(i-m)**2 for i in y])
+    ss_res = sum([(y[i]-predict(alpha,beta,x[i]))**2 for i in range(len(y)) ])
+    #print ss_tot, ss_res
+    return 1-(ss_res/ss_tot)
+
 if __name__ == "__main__":
     # Here, we load the boston dataset
     boston = csv.reader(open('boston.csv'))  # The boston housing dataset in csv format
@@ -45,43 +75,6 @@ if __name__ == "__main__":
         R2 = r_squared(alpha,beta,x,target)  # Fill with the real value from your code
         fout.write('%f,%f,%f\n' % (alpha, beta, R2))  # One line per variable
     fout.close()
-
-
-# In[100]:
-
-
-def predict(alpha, beta, x_i):
-    return alpha+beta*x_i
-
-def mean(x):
-    return float(sum(x))/len(x)
-            
-def variance(x):
-    x_2 = [i**2 for i in x]
-    return float(mean(x_2)-mean(x)**2)
-def covariance(x,y):
-    x_y = [i*j for i,j in zip(x,y)]
-    return mean(x_y)-mean(x)*mean(y)
-
-def correlation(x,y):
-    return float(covariance(x,y)/(math.sqrt(variance(x))*math.sqrt(variance(y))))
-
-def linear_regression_least_squares(x,y):
-    beta = covariance(x,y)/variance(x)
-    alpha = mean(y)-beta*mean(x)
-    return float(alpha), float(beta)
-    
-def r_squared(alpha, beta, x, y):
-    ss_tot = 0
-    ss_res = 0
-    m = mean(y)
-    ss_tot = sum([(i-m)**2 for i in y])
-    ss_res = sum([(y[i]-predict(alpha,beta,x[i]))**2 for i in range(len(y)) ])
-    #print ss_tot, ss_res
-    return 1-(ss_res/ss_tot)
-
-
-# In[ ]:
 
 
 
